cce89492c8aca8f1fb9c0c8b05b83592e75b12fa
CarlosCordoba96 <Carlos.Cordoba@alu.uclm.es>
2020-10-11 23:06:06 +0200

    Add config and scripts to create a kind cluster

---
+++ KindConfig/start-cluster-withIngress.sh
@@ -0,0 +1,5 @@
+kind create cluster --config=kind-config.yaml
+
+sleep 10
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
